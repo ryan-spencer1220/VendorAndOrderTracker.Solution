2d541e2399731b55a8c8496bb60eea489a33af8d
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-03-04 08:44:40 -0800

    Pass VendorConstructor test

---
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Name", "Description", 555);
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
---
+++ VendorOrderTracker/Models/Vendor.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int PhoneNumber { get; set; }
+
+    public int Id { get;}
+    // public List<Order> Orders { get; set; }
+
+    public Vendor(string vendorName, string description, int phoneNumber)
+    {
+      Name = vendorName;
+      Description = description;
+      PhoneNumber = phoneNumber;
+      _instances.Add(this);
+      Id = _instances.Count;
+      // Orders = new List<Order>{};
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId -1];
+    }
+
+    // public void AddItem(Order order)
+    // {
+    //   Orders.Add order);
+    // }
+  }
+}
