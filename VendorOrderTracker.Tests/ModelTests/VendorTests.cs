a9c7a768661583584e78931913a8ec65c7538a1b
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-03-04 09:00:20 -0800

    Pass GetAll test to retrive a list of all vendors

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -38,5 +38,14 @@ namespace VendorOrderTracker.Tests
       int result = newVendor.Id;
       Assert.AreEqual(1, result);
     }
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      Vendor newVendor1 = new Vendor("Name1", "Description1", 444-444-4444);
+      Vendor newVendor2 = new Vendor("Name2", "Description2", 555-555-5555);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
